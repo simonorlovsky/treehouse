06373130a1e4b2b0357704805ca62560f9715f23
simonorlovsky <simon.orlovsky@yahoo.com>
2016-06-29 20:30:29 -0500

    began network programming tutorial

---
+++ iOS-NetworkProgrammingSwift2-StormyStarterFiles/Stormy/CurrentWeather.swift
@@ -0,0 +1,18 @@
+//
+//  CurrentWeather.swift
+//  Stormy
+//
+//  Created by Simon Orlovsky on 6/28/16.
+//  Copyright © 2016 Treehouse. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct CurrentWeather {
+    let temperature: Double
+    let humidity: Double
+    let precipitationProbability: Double
+    let summary: String
+    let image: UIImage
+}
---
+++ iOS-NetworkProgrammingSwift2-StormyStarterFiles/Stormy/WeatherIcon.swift
@@ -0,0 +1,22 @@
+//
+//  WeatherIcon.swift
+//  Stormy
+//
+//  Created by Simon Orlovsky on 6/28/16.
+//  Copyright © 2016 Treehouse. All rights reserved.
+//
+
+import Foundation
+
+enum WeatherIcon: String {
+    case ClearDay = "clear-day"
+    case ClearNight = "clear-night"
+    case Rain = "rain"
+    case Snow = "snow"
+    case Sleet = "sleet"
+    case Wind = "wind"
+    case Fog = "fog"
+    case Cloudy = "cloudy"
+    case PartlyCloudyDay = "party-cloudy-day"
+    case PartlyCloudyNight = "party-cloudy-night"
+}
